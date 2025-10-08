1ce914604133581b43c0f1fc878fe615b58f42cc
Anas Habib <anashabib36136@gmail.com>
2025-10-08 13:55:01 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,29 @@
+const accountId = 114433322
+let acccountEmail ="anashabib36136@gmail.com"
+var accountPassword ="123456"
+accountCity = "Lahore"
+let accountState;
+
+
+// accountId = 1234 // not allowed
+console.log(accountId)
+
+accountPassword = "1"
+acccountEmail = "hamza@gmail.com"
+accountCity = "Multan"
+
+// console.log(accountPassword)  // Ya time taking process ha, bar bar print karna parta ha 
+// console.log(acccountEmail)
+// console.log(accountCity)
+
+console.table([accountId, acccountEmail, accountPassword, accountCity, accountState])
+
+
+// console.log("Anas Habib");   //console.log mali to asi print kro...  
+// var, let const, mali to memery mi assign karo
+//const accountId = 114433322
+// memory name (accountId) ha,  Ya value assign karni ha ya (114433322).
+// const means: ka matlab hota hai → constant → value fix hai → dobara assign nahi kar sakte ha.
+/*
+perfer not use to use var, because of issue in block scope functionals 
+*/
--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,16 @@
-console.log("Anas Habib");
+const accountId =  12345;
+let accountEmail = "anas@gmail.com";
+var accountPassword = "11223344";
+accountCity = "Lahore";
+let accountState;
+
+
+// accountId = 1;
+// console.log(accountId);
+
+// accountId = 1;
+acccountEmail = "hamzahabib@gmail.com";
+accountPassword = "123";
+accountCity = "multan";
+
+console.table([accountId, acccountEmail, accountPassword, accountCity, accountStates]);
